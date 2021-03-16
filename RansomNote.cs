07cd05d3315de48d7931db43f6fb5fdbb062ed78
nikhil7869 <80478297+nikhil7869@users.noreply.github.com>
2021-03-16 14:35:41 +0530

    Create RansomNote.cs

---
+++ RansomNote.cs
@@ -0,0 +1,140 @@
+/*
+
+Problem Statement
+
+Harold is a kidnapper who wrote a ransom note, but now he is worried it will be traced back to him through his handwriting. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use only whole words available in the magazine. He cannot use substrings or concatenation to create the words he needs.
+
+Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+
+Example
+ = "attack at dawn"  = "Attack at dawn"
+
+The magazine has all the right words, but there is a case mismatch. The answer is .
+
+Function Description
+
+Complete the checkMagazine function in the editor below. It must print  if the note can be formed using the magazine, or .
+
+checkMagazine has the following parameters:
+
+string magazine[m]: the words in the magazine
+string note[n]: the words in the ransom note
+Prints
+
+string: either  or , no return value is expected
+Input Format
+
+The first line contains two space-separated integers,  and , the numbers of words in the  and the , respectively.
+The second line contains  space-separated strings, each .
+The third line contains  space-separated strings, each .
+
+Constraints
+
+.
+Each word consists of English alphabetic letters (i.e.,  to  and  to ).
+Sample Input 0
+
+6 4
+give me one grand today night
+give one grand today
+Sample Output 0
+
+Yes
+Sample Input 1
+
+6 5
+two times three is not four
+two times two is four
+Sample Output 1
+
+No
+Explanation 1
+
+'two' only occurs once in the magazine.
+
+Sample Input 2
+
+7 4
+ive got a lovely bunch of coconuts
+ive got some coconuts
+Sample Output 2
+
+No
+Explanation 2
+
+Harold's magazine is missing the word .
+*/
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(string[] magazine, string[] note) {
+        Dictionary<string, int> d = new Dictionary<string, int>();
+        bool isValidMagazine = true;
+        for(int i = 0; i < magazine.Length; i++)
+        {
+            if(d.ContainsKey(magazine[i]))
+                d[magazine[i]]++;
+            else 
+                d[magazine[i]] = 1; 
+        }
+        foreach(string s in note)
+        {
+            if(!d.ContainsKey(s))
+            {
+                isValidMagazine = false;
+                break;
+            }
+            else
+            {
+                if(d[s] > 0)
+                    d[s]--;
+                else
+                {
+                    isValidMagazine = false;
+                    break;
+                }
+                    
+            }
+        }
+        
+        if(isValidMagazine)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+
+    }
+
+    static void Main(string[] args) {
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        string[] magazine = Console.ReadLine().Split(' ');
+
+        string[] note = Console.ReadLine().Split(' ');
+
+        checkMagazine(magazine, note);
+    }
+}
